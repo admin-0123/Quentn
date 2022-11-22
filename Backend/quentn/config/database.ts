43963e9b5f399ddeaeea03962551fbabdaeeae74
Achiever H <williamjackson0123@outlook.com>
2022-11-21 23:14:02 -0300

    postgres

--- Backend/quentn/config/database.ts
+++ Backend/quentn/config/database.ts
@@ -2,7 +2,7 @@ export default ({ env }) => ({
   connection: {
     client: 'postgres',
     connection: {
-      host: env('DATABASE_HOST', 'backend'),
+      host: env('DATABASE_HOST', 'postgres'),
       port: env.int('DATABASE_PORT', 5432),
       database: env('DATABASE_NAME', 'quentn'),
       user: env('DATABASE_USERNAME', 'postgres'),
